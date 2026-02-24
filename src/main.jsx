e92ed8a3b1a6b8d91df7cb31d803d1ddc802a4f7
Katelyn Wilson <kking927@hotmail.com>
2026-02-24 16:39:26 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'      // match App.jsx exactly
-import './index.css'         // global/reset styles
-import './app.css'           // component styles (optional)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
